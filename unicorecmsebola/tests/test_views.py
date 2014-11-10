c58205005e81e9e6388789e577ccb9295e1dc311
Milton Madanda <madandat@gmail.com>
2014-11-10 22:27:25 +0200

    add test for the footer

--- unicorecmsebola/tests/test_views.py
+++ unicorecmsebola/tests/test_views.py
@@ -60,3 +60,31 @@ class TestViews(UnicoreTestCase):
         self.assertTrue(
             '<p>this is the description text</p>' in
             resp.body)
+
+    def test_footer_page(self):
+        self.workspace.setup_custom_mapping(Page, {
+            'properties': {
+                'slug': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                },
+                'language': {
+                    'type': 'string',
+                }
+            }
+        })
+
+        self.create_categories(self.workspace, count=1)
+
+        intro_page = Page({
+            'title': 'Footer page', 'language': 'eng_UK',
+            'description': 'this is the description text',
+            'slug': 'site-footer', 'content': 'this is the body of work',
+            'position': 0, 'modified_at': datetime.utcnow().isoformat()})
+        self.workspace.save(intro_page, 'save footer')
+        self.workspace.refresh_index()
+
+        resp = self.app.get('/', status=200)
+        self.assertTrue(
+            '<span>this is the body of work</span>' in
+            resp.body)
