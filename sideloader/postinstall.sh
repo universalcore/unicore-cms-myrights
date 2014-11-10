5a1c3c209165b362167868080f08dc280e1372b3
Milton Madanda <madandat@gmail.com>
2014-11-10 23:43:22 +0200

    add mappings

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -8,6 +8,6 @@ ini_files="ebola.*.ini"
 
 for ini in $ini_files
 do
-    `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true
-    `which eg-tools` resync -c $ini -m unicore.content.models.Page -r true
+    `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true -f mappings/category.mapping.json
+    `which eg-tools` resync -c $ini -m unicore.content.models.Page -r true -f mappings/page.mapping.json
 done
