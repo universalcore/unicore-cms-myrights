0dee28b1967c379da13d005b9257a1d826da8847
Milton Madanda <madandat@gmail.com>
2014-11-10 23:34:40 +0200

    trash index on deploy

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -8,6 +8,6 @@ ini_files="ebola.*.ini"
 
 for ini in $ini_files
 do
-    `which eg-tools` resync -c $ini -m unicore.content.models.Category
-    `which eg-tools` resync -c $ini -m unicore.content.models.Page
+    `which eg-tools` resync -c $ini -m unicore.content.models.Category -f true
+    `which eg-tools` resync -c $ini -m unicore.content.models.Page -f true
 done
