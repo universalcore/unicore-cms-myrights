869a9ea5c0dd23821da71d943c967bf77a700599
Milton Madanda <madandat@gmail.com>
2014-12-01 10:14:32 +0200

    allow changing of header image

--- unicorecmsebola/tests/test_views.py
+++ unicorecmsebola/tests/test_views.py
@@ -3,7 +3,7 @@ from pyramid import testing
 
 from cms.tests.base import UnicoreTestCase
 from unicorecmsebola import main
-from unicore.content.models import Page
+from unicore.content.models import Page, Localisation
 from webtest import TestApp
 
 
@@ -19,7 +19,8 @@ class TestViews(UnicoreTestCase):
             'cache.regions': 'long_term, default_term',
             'cache.long_term.expire': '1',
             'cache.default_term.expire': '1',
-            'pyramid.default_locale_name': 'eng_UK',
+            'pyramid.default_locale_name': 'eng_GB',
+            'thumbor.security_key': 'sample-security-key',
         }
         self.config = testing.setUp(settings=settings)
         self.app = TestApp(main({}, **settings))
@@ -36,15 +37,28 @@ class TestViews(UnicoreTestCase):
                 }
             }
         })
+        self.workspace.setup_custom_mapping(Localisation, {
+            'properties': {
+                'locale': {
+                    'type': 'string',
+                    'index': 'not_analyzed',
+                }
+            }
+        })
 
         self.create_categories(self.workspace, count=1)
 
         intro_page = Page({
-            'title': 'Homepage Intro Title', 'language': 'eng_UK',
+            'title': 'Homepage Intro Title', 'language': 'eng_GB',
             'description': 'this is the description text',
             'slug': 'homepage-intro', 'content': 'this is the body of work',
             'position': 0, 'modified_at': datetime.utcnow().isoformat()})
         self.workspace.save(intro_page, 'save intro')
+        localisation = Localisation({
+            'locale': 'eng_GB',
+            'image_host': 'http://some.site.com',
+            'image': 'some-uuid'})
+        self.workspace.save(localisation, 'save localisation')
         self.workspace.refresh_index()
 
         resp = self.app.get('/', status=200)
@@ -53,6 +67,9 @@ class TestViews(UnicoreTestCase):
             resp.body)
         self.assertTrue(
             '<p>this is the description text</p>' in
+            resp.body)
+        self.assertTrue(
+            '<img alt="Ebola Information" src="http://some.site.com/VNlJN07VKnfaB6k1imziAts4n0o=/320x0/some-uuid"/>' in
             resp.body)
 
         resp = self.app.get('/', status=200)
@@ -83,7 +100,7 @@ class TestViews(UnicoreTestCase):
         self.create_categories(self.workspace, count=1)
 
         intro_page = Page({
-            'title': 'Footer page', 'language': 'eng_UK',
+            'title': 'Footer page', 'language': 'eng_GB',
             'description': 'this is the description text',
             'slug': 'site-footer', 'content': 'this is the body of work',
             'position': 0, 'modified_at': datetime.utcnow().isoformat()})
