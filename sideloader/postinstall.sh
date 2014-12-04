145b4b1c074b00c2df1f23c8bc6979b1e1ed8d32
Milton Madanda <madandat@gmail.com>
2014-12-04 11:45:58 +0200

    add localisation mapping

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -10,4 +10,5 @@ for ini in $ini_files
 do
     `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true -f mappings/category.mapping.json
     `which eg-tools` resync -c $ini -m unicore.content.models.Page -f mappings/page.mapping.json
+    `which eg-tools` resync -c $ini -m unicore.content.models.Localisation -f mappings/localisation.mapping.json
 done
