d4baf6631188eec4ef2d89adfc717f34fa400649
Simon de Haan <simon@praekeltfoundation.org>
2015-07-03 09:13:20 +0200

    fix sideloader postinstall

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -4,11 +4,11 @@ cd "${INSTALLDIR}/${REPO}/"
 
 $pip install -e "${INSTALLDIR}/${REPO}/"
 
-ini_files="myrights_*.ini"
+ini_files="/var/praekelt/unicore-mc/project/configs/frontend_settings/myrights_*.ini"
 
 for ini in $ini_files
 do
-    `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true -f mappings/category.mapping.json
-    `which eg-tools` resync -c $ini -m unicore.content.models.Page -f mappings/page.mapping.json
-    `which eg-tools` resync -c $ini -m unicore.content.models.Localisation -f mappings/localisation.mapping.json
+    `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true -f "${INSTALLDIR}/${REPO}/mappings/category.mapping.json"
+    `which eg-tools` resync -c $ini -m unicore.content.models.Page -f "${INSTALLDIR}/${REPO}/mappings/page.mapping.json"
+    `which eg-tools` resync -c $ini -m unicore.content.models.Localisation -f "${INSTALLDIR}/${REPO}/mappings/localisation.mapping.json"
 done
