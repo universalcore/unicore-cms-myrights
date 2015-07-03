8a3e21c824838bc1dc37efc42a7be60b5e0eeaec
Simon de Haan <simon@praekeltfoundation.org>
2015-07-03 12:49:56 +0200

    make sure uWSGI reloads the app on deploy

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -11,4 +11,5 @@ do
     `which eg-tools` resync -c $ini -m unicore.content.models.Category -r true -f "${INSTALLDIR}/${REPO}/mappings/category.mapping.json"
     `which eg-tools` resync -c $ini -m unicore.content.models.Page -f "${INSTALLDIR}/${REPO}/mappings/page.mapping.json"
     `which eg-tools` resync -c $ini -m unicore.content.models.Localisation -f "${INSTALLDIR}/${REPO}/mappings/localisation.mapping.json"
+    touch $ini
 done
