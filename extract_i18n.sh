5d1f8a72ed85a1fd017e1a8f06be60536b02946d
Simon de Haan <simon@praekeltfoundation.org>
2015-07-03 14:19:59 +0200

    new translation stuff

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 find . -name '*.mo' -delete
-mkdir -p unicorecmsebola/locale
+mkdir -p unicorecmsmyrights/locale
 
-pot-create -o unicorecmsebola/locale/unicorecmsebola.pot unicorecmsebola/
+pot-create -o unicorecmsmyrights/locale/unicorecmsmyrights.pot unicorecmsmyrights/
 
-declare -a arr=("eng_GB" "swa_TZ" "fre_FR" "por_PT" "por_AO")
+declare -a arr=("eng_GB" "hin_IN")
 
 for lang in "${arr[@]}"
 do
-    mkdir -p "unicorecmsebola/locale/""$lang""/LC_MESSAGES"
+    mkdir -p "unicorecmsmyrights/locale/""$lang""/LC_MESSAGES"
 
-    if [ ! -f "unicorecmsebola/locale/""$lang""/LC_MESSAGES/unicorecmsebola.po" ]; then
-        msginit -l $lang -i unicorecmsebola/locale/unicorecmsebola.pot -o unicorecmsebola/locale/$lang/LC_MESSAGES/unicorecmsebola.po
+    if [ ! -f "unicorecmsmyrights/locale/""$lang""/LC_MESSAGES/unicorecmsmyrights.po" ]; then
+        msginit -l $lang -i unicorecmsmyrights/locale/unicorecmsmyrights.pot -o unicorecmsmyrights/locale/$lang/LC_MESSAGES/unicorecmsmyrights.po
     fi
 
-    msgmerge --update unicorecmsebola/locale/$lang/LC_MESSAGES/unicorecmsebola.po unicorecmsebola/locale/unicorecmsebola.pot
-    msgfmt unicorecmsebola/locale/$lang/LC_MESSAGES/*.po -o unicorecmsebola/locale/$lang/LC_MESSAGES/unicorecmsebola.mo
+    msgmerge --update unicorecmsmyrights/locale/$lang/LC_MESSAGES/unicorecmsmyrights.po unicorecmsmyrights/locale/unicorecmsmyrights.pot
+    msgfmt unicorecmsmyrights/locale/$lang/LC_MESSAGES/*.po -o unicorecmsmyrights/locale/$lang/LC_MESSAGES/unicorecmsmyrights.mo
 done
